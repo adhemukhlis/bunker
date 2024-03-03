34b298f06bf9a5d66b3506511b9ed336e9a78da0
adhemukhlis <adhemukhlis@gmail.com>
2024-03-03 15:15:41 +0700

    add yarn lock

--- src/utils/session/index.js
+++
@@ -1,8 +0,0 @@
-import { getIronSession } from "iron-session";
-import { cookies } from "next/headers";
-import sessionOptions from "./session-options";
-const getIronSessionHandler = async () => {
-  const session = await getIronSession(cookies(), sessionOptions);
-  return session;
-};
-export default getIronSessionHandler;
