29f5edda52f244a4c4ffd24b01d17144ee83ba48
adhemukhlis <adhemukhlis@gmail.com>
2024-03-03 15:57:08 +0700

    update

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,8 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
+    <main>
+      <div>
         <h1>Mukhlis Adhe Purwanto</h1>
       </div>
     </main>
